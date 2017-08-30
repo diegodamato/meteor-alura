bd1a0e2c024dd5cd3b251f929f33e9f5c68fcf20
diego <diego.damato>
2017-08-30 18:35:06 -0300

    Finalizando aula1

---
+++ client/lista/lista.js
@@ -0,0 +1,11 @@
+Template.lista.helpers({
+    
+    tarefas: function(){
+        return [
+            {nome: "Ir ao mercado"},
+            {nome: "Comprar sorvete"},
+            {nome: "Estudar"}
+        ];
+    }
+
+})
